f5239a05709732fd22505a6a50e3886b39f1b222
kabwefelix <kabwefelix10@gmail.com>
2026-03-24 14:26:49 +0800

    Delete manifold directory

--- manifold/manifold/paths.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Default to the directory above 'manifold/' package if MANIFOLD_HOME is not set
-_DEFAULT_HOME = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-MANIFOLD_HOME = os.environ.get("MANIFOLD_HOME", _DEFAULT_HOME)
-
-def get_path(filename: str) -> str:
-    """Returns the absolute path for a given filename based on MANIFOLD_HOME."""
-    return os.path.join(MANIFOLD_HOME, filename)
